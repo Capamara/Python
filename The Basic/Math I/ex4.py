f9a59793513316c812d664f300725bc37217fb32
Gabriel Amara <80047823+Capamara@users.noreply.github.com>
2021-09-09 08:57:49 -0300

    Add files via upload

---
+++ The Basic/Math I/ex4.py
@@ -0,0 +1,10 @@
+print("Bem vindo ao posto X")
+precolitro=float(input("\nColoque o preco da gasolina por litro:"))
+dinheiro=float(input("\nCom quantos reais deseja abastecer:"))
+
+resultado_1= dinheiro/precolitro
+
+print("\nVoce conseguira abastecer", resultado_1,"L")
+
+input("\nPress Enter to exit")
+
